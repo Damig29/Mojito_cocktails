8f62e46a2e675621f4f1a21d2b93270454e480cc
unknown <Damig29>
2025-09-16 17:02:52 +0100

    implement the first part of the hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,27 @@
 import { navLinks } from '../../constants/index'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Navbar = () => {
+  useGSAP(() => {
+    // GSAP animation code can be added here
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top',
+      }
+    });
+
+    navTween.fromTo('nav', { backgroundColor: 'transparent' }, 
+      {
+        backgroundColor: '#00000050', 
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut'
+      }
+    );
+  })
+
   return (
     <nav>
       <div>
